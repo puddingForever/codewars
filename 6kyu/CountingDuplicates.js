f225d8651452f599cd9b2519499829e2af2be194
jje <developerjieun94@gmail.com>
2024-05-20 17:36:06 +0900

    [5/20]

---
+++ 6kyu/CountingDuplicates.js
@@ -0,0 +1,54 @@
+// Count the number of Duplicates
+// Write a function that will return the count of distinct case-insensitive alphabetic characters and numeric digits that occur more than once in the input string. The input string can be assumed to contain only alphabets (both uppercase and lowercase) and numeric digits.
+
+// Example
+// "abcde" -> 0 # no characters repeats more than once
+// "aabbcde" -> 2 # 'a' and 'b'
+// "aabBcde" -> 2 # 'a' occurs twice and 'b' twice (`b` and `B`)
+// "indivisibility" -> 1 # 'i' occurs six times
+// "Indivisibilities" -> 2 # 'i' occurs seven times and 's' occurs twice
+// "aA11" -> 2 # 'a' and '1'
+// "ABBA" -> 2 # 'A' and 'B' each occur twice
+
+// my solution
+function duplicateCount(text) {
+  const arr = text.split('').map(function (e) {
+    if (isNaN(e)) {
+      // 문자일때
+      return e.toUpperCase();
+    } else {
+      return e;
+    }
+  });
+
+  var myMap = new Map();
+
+  for (let el of arr) {
+    if (myMap.has(el)) {
+      myMap.set(el, false);
+    } else {
+      myMap.set(el, true);
+    }
+  }
+
+  let count = 0;
+  for (let value of myMap.values()) {
+    if (!value) count++;
+  }
+
+  return count;
+}
+
+// other solution
+// ^ refers to nagative in most cases, however, if it is surrounded by () capture group ,
+// it refers to all the characters
+// \+1 is a back reference that checks the previous character
+// so basically it returns the length of the duplicates array .
+function duplicateCounting(text) {
+  return text
+    .toLowerCase()
+    .split('')
+    .sort()
+    .join('')
+    .match(/([^])\1+/g).length;
+}
