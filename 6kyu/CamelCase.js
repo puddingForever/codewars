49a82fdd3d6d76b17c91b75a468ee734e17ecd19
jje <developerjieun94@gmail.com>
2024-05-21 16:04:55 +0900

    [5/20]

--- 6kyu/CamelCase.js
+++ 6kyu/CamelCase.js
@@ -4,3 +4,28 @@
 // "camelCasing"  =>  "camel Casing"
 // "identifier"   =>  "identifier"
 // ""             =>  ""
+// my solution
+function camelCase(str) {
+  let strArr = str.split('');
+
+  for (let i = 0; i < strArr.length; i++) {
+    if (strArr[i] > 'A' && strArr[i] < 'Z') {
+      strArr[i] = ' ' + strArr[i];
+    }
+  }
+
+  return strArr.join('');
+}
+
+// other solution
+function camelCase(str) {
+  let strArr = str.split('').map(function (el) {
+    if (el === el.toUpperCase()) {
+      return ' ' + el;
+    }
+
+    return el;
+  });
+
+  return strArr.join('');
+}
