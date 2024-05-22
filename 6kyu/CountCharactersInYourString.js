e3c4a2f73154284935c8b7d108947b2ef63623fc
jje <developerjieun94@gmail.com>
2024-05-22 13:26:38 +0900

    [05/22] 5 kyu complete

--- 6kyu/CountCharactersInYourString.js
+++ 6kyu/CountCharactersInYourString.js
@@ -2,9 +2,18 @@
 
 // What if the string is empty? Then the result should be empty object literal, {}.
 
+// my solution
 function countCharactersInYourString(str) {
   let result = {};
   str.split('').map((el) => (result[el] = result[el] + 1 || 1));
-
   return result;
+}
+
+// other
+function count(str) {
+  var count = {};
+  str.split('').forEach(function (s) {
+    count[s] ? count[s]++ : (count[s] = 1);
+  });
+  return count;
 }
