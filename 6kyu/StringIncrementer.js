21a4b3a311401f788e082eec36bba27d2dc548e4
jje <developerjieun94@gmail.com>
2024-06-19 11:04:19 +0900

    [06/19]

---
+++ 6kyu/StringIncrementer.js
@@ -0,0 +1,24 @@
+// Your job is to write a function which increments a string, to create a new string.
+// If the string already ends with a number, the number should be incremented by 1.
+// If the string does not end with a number. the number 1 should be appended to the new string.
+// Examples:
+// foo -> foo1
+// foobar23 -> foobar24
+// foo0042 -> foo0043
+// foo9 -> foo10
+// foo099 -> foo100
+// Attention: If the number has leading zeros the amount of digits should be considered.
+
+function incrementString(input) {
+  if (isNaN(parseInt(input[input.length - 1]))) return input + '1';
+
+  // match -> 정규식 전체와 매치된 문자열
+  // p1 -> 첫번째 매칭그룹 ( 0* )
+  // p2 -> 두번쨰 매칭그룹 ([0-9]+$)
+  return input.replace(/(0*)([0-9]+$)/, function (match, p1, p2) {
+    var up = parseInt(p2) + 1;
+
+    // p1.slice(0,-1) 배열의 마지막에서 첫번째까지
+    return up.toString().length > p2.length ? p1.slice(0, -1) + up : p1 + up;
+  });
+}
